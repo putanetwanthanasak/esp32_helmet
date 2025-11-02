b4e24f212670742b6b71de89d8fb0a717e53c645
PUTANET WANTHANASAK <putanet.want@kmutt.ac.th>
2025-11-02 22:30:01 +0700

    Add Line_messagingAPI Feature

---
+++ src/secretsConfig.h
@@ -0,0 +1,5 @@
+#pragma once
+#define LINE_TOKEN  "QxLVAgZdMU+4DYuuKyDf4CT1J9BSZ+XKGwD9As5aVk+BFUZyQCP5yJlp5TBk2SHz4DrwQjQhFdwY7bSbFulNYIwee/5Uxh5h0E+OqkIUYZhWLUzdWPkLIV3C1WNx1PKi0hO72ADIRcTrSq7v7PBl1gdB04t89/1O/w1cDnyilFU="   // จาก LINE Messaging API
+#define LINE_USER   "U9d05625ae1d2e1edc7e9a0591aea34f2" // userId (หรือ groupId/roomId)
+#define WIFI_SSID "Saransuk_B420_2.4G"
+#define WIFI_PASS "35353059"
---
+++ src/secretsConfig_example.h
@@ -0,0 +1,5 @@
+#pragma once
+#define LINE_TOKEN  "YOUR_CHANNEL_ACCESS_TOKEN"   // จาก LINE Messaging API
+#define LINE_USER   "Uxxxxxxxxxxxxxxxxxxxxxxxxxx" // userId (หรือ groupId/roomId)
+#define WIFI_SSID "YOUR_WIFI"
+#define WIFI_PASS "YOUR_PASS"
