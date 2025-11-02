5cc3cb05f89da287f08dc5abaa91c714e1eb6580
PUTANET WANTHANASAK <putanet.want@kmutt.ac.th>
2025-11-02 22:50:21 +0700

    feature/line_alert

--- src/secretsConfig.h
+++ src/secretsConfig.h
@@ -1,5 +1,5 @@
 #pragma once
-#define LINE_TOKEN  "QxLVAgZdMU+4DYuuKyDf4CT1J9BSZ+XKGwD9As5aVk+BFUZyQCP5yJlp5TBk2SHz4DrwQjQhFdwY7bSbFulNYIwee/5Uxh5h0E+OqkIUYZhWLUzdWPkLIV3C1WNx1PKi0hO72ADIRcTrSq7v7PBl1gdB04t89/1O/w1cDnyilFU="   // จาก LINE Messaging API
+#define LINE_TOKEN  "fhbDPjcBRmHU/q3UGR2CL3ms9nQYim2T0n3OweqKYX3QCrsfoxERmYccDZq/EFzZ4DrwQjQhFdwY7bSbFulNYIwee/5Uxh5h0E+OqkIUYZg3bMsNbcFq5sny6JlkXdiGA1Ql3RyggKKf3OIyo79FvgdB04t89/1O/w1cDnyilFU="   // จาก LINE Messaging API
 #define LINE_USER   "U9d05625ae1d2e1edc7e9a0591aea34f2" // userId (หรือ groupId/roomId)
 #define WIFI_SSID "Saransuk_B420_2.4G"
 #define WIFI_PASS "35353059"
